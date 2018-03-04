ff3f424cc8d462c764532d33a4250a6a3724c1b4
Eric Norris <erictnorris@gmail.com>
2018-03-03 21:02:11 -0500

    Add chart and table for all symbols analysis

--- lfa/htmlgen.py
+++ lfa/htmlgen.py
@@ -3,9 +3,26 @@ from .analyze import AnalysisResults
 from jinja2 import Environment, FileSystemLoader
 
 def htmlgen(counts: AnalysisResults, out_dir: str):
-	env = Environment(loader=FileSystemLoader('templates'))
+    env = Environment(loader=FileSystemLoader('templates'))
 
-	index_template = env.get_template('index.jinja2')
+    index_template = env.get_template('index.jinja2')
 
-	with open(out_dir + '/index.html', 'w') as index_file:
-		index_template.stream().dump(index_file)
+    with open(out_dir + '/index.html', 'w') as index_file:
+        index_template.stream(
+            all_symbols_count_desc=counts.all_symbols.most_common(),
+            all_symbols_relative_freq=calculate_all_symbols_relative_frequency(counts)
+        ).dump(index_file)
+
+
+def calculate_all_symbols_relative_frequency(counts: AnalysisResults) -> list:
+    total = sum(counts.all_symbols.values())
+
+    percentages = sorted([
+        (symbol, count / total) for symbol, count in counts.letters.items()
+    ])
+
+    percentages.append(('[[:digit:]]', counts.all_symbols['numbers'] / total))
+    percentages.append(('[[:punct:]]', counts.all_symbols['punctuation'] / total))
+    percentages.append(('[ ]', counts.all_symbols['space'] / total))
+
+    return percentages
